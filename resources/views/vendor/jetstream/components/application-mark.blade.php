30b2a364bba892588844f5a74f742b1cdef6b746
MORJAN <janluy.moreno@gmail.com>
2023-01-09 20:04:44 -0500

    commitment to the project plus system, with the base of this

---
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <img src="{{asset('images/icono.png')}}" alt="icono-paola" width="160" height="160" style="filter:drop-shadow(1px 1px 2px black);">
+</div>
---
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -0,0 +1,15 @@
+<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+    <div>
+        <x-jet-application-logo class="block h-12 w-auto inset-x-auto"/>
+    </div>
+
+    <div class="mt-8 text-2xl">
+        {{__('Welcome_title')}}
+    </div>
+
+    <div class="mt-6 text-gray-500">
+        {{__('Welcome_subtitle')}}
+    </div>
+</div>
+
+
